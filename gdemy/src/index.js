6725b26ed9a945f086ef9a2b3c7fccd160dd7bb4
Subhajit <subhabera211@gmail.com>
2023-09-04 20:44:24 +0530

    Update index.js

--- gdemy/src/index.js
+++ gdemy/src/index.js
@@ -1,8 +1,8 @@
-import { ColorModeScript } from '@chakra-ui/react';
+import { ColorModeScript,ChakraProvider,theme } from '@chakra-ui/react';
 import React, { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import App from './App';
-
+import ColorModeSwitcher from './ColorModeSwitcher';
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
@@ -10,7 +10,11 @@ const root = ReactDOM.createRoot(container);
 root.render(
   <StrictMode>
     <ColorModeScript />
+    <ChakraProvider theme={theme}>
+    <ColorModeSwitcher/>
     <App />
+    </ChakraProvider>
+    
   </StrictMode>
 );
 
